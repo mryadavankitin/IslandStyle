9e2bd96a723b2e3e31fc222df2cb2ff408fa3e8b
mryadavankitin <mryadavankitin@gmail.com>
2021-04-17 18:59:30 +0530

    hellp

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const mysql = require('mysql')
+
+//pass: Ga@5xIfjxWVFbmVT
+//create connection
+const db = mysql.createConnection({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database : process.env.database,
+    port:process.env.port
+
+})
+
+//connect to mySQL
+
+db.connect(err =>{
+
+    if(err){
+        
+        throw err
+
+    }
+console.log("my sql connected")
+})
+
+const app = express()
+
+app.get("/getskudetails",(req,res) => {
+
+
+    var skuString = "'" +  req.query.skuID + "'";
+    var dbQyery = "Select sspd.name productName,sspa.name sizes, ssp.id, ssp.price,ssp.sku from sc_shop_product ssp, sc_shop_product_description sspd,sc_shop_product_attribute sspa where ssp.id = sspd.product_id AND ssp.id= sspa.product_id AND sspd.product_id=sspa.product_id AND ssp.sku=" + skuString
+    console.log("hello DB query is",dbQyery);
+    db.query(dbQyery, (err, rows) => {
+        if(err) throw err;
+        console.log('The data from users table are: \n', rows);
+        // db.end();
+        if(rows.length >0){
+            var sizeAttr=[]
+            for(var i=0; i < rows.length ; i++){
+
+               sizeAttr.push(rows[i].sizes)
+            }
+
+            var ResponeDict= rows[0];
+            console.log("sixe att above", ResponeDict)
+
+            ResponeDict.sizes= sizeAttr;
+            console.log("sixe att", ResponeDict)
+            return res.status(200).send(rows)
+
+        }
+        else{
+            return res.status(404).send("no data found")
+        }
+
+    });
+});
+
+const port = process.env.SYSTEM_PORT || 8080
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
+
+// app.set("port", process.env.SYSTEM_PORT || 8080);
+
+// http.createServer(app).listen(app.get('port'), function() {
+//     console.log('Server is running at port' + process.env.SYSTEM_PORT);
+
+// });
+
+
+
+// app.listen(process.env.SYSTEM_PORT, () => {
+// });

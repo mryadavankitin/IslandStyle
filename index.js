edb3ffe6c286a3e2ac27721896fed74692ea7278
mryadavankitin <mryadavankitin@gmail.com>
2021-04-21 18:56:16 +0530

    Config changes

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const mysql = require('mysql')
 
 //pass: Ga@5xIfjxWVFbmVT
 //create connection
-// const db = mysql.createConnection({
-//     host: process.env.host,
-//     user: process.env.user,
-//     password: process.env.password,
-//     database : process.env.database,
-//     port:process.env.port
-
-// })
-
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "styleisland",
-    password: "Ga@5xIfjxWVFbmVT",
-    database : "styleisland",
-    port:"3306"
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database : process.env.database,
+    port:process.env.port
 
 })
+
+// const db = mysql.createConnection({
+//     host: "localhost",
+//     user: "styleisland",
+//     password: "Ga@5xIfjxWVFbmVT",
+//     database : "styleisland",
+//     port:"3306"
+
+// })
 
 //connect to mySQL
 

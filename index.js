73ea7cf1a26e7c24d9b4e1c64b6d8633339139ae
mryadavankitin <mryadavankitin@gmail.com>
2021-04-21 18:55:33 +0530

    added fix for sizes

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const mysql = require('mysql')
 
 //pass: Ga@5xIfjxWVFbmVT
 //create connection
-const db = mysql.createConnection({
-    host: process.env.host,
-    user: process.env.user,
-    password: process.env.password,
-    database : process.env.database,
-    port:process.env.port
-
-})
-
 // const db = mysql.createConnection({
-//     host: "localhost",
-//     user: "styleisland",
-//     password: "Ga@5xIfjxWVFbmVT",
-//     database : "styleisland",
-//     port:"3306"
+//     host: process.env.host,
+//     user: process.env.user,
+//     password: process.env.password,
+//     database : process.env.database,
+//     port:process.env.port
 
 // })
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "styleisland",
+    password: "Ga@5xIfjxWVFbmVT",
+    database : "styleisland",
+    port:"3306"
+
+})
 
 //connect to mySQL
 
@@ -40,7 +40,7 @@ const app = express()
 
 app.get("/getskudetails",(req,res) => {
 
-
+    
     var skuString = "'" +  req.query.skuID + "'";
     var dbQyery = "Select sspd.name productName,sspa.name sizes, ssp.id, ssp.price,ssp.sku from sc_shop_product ssp, sc_shop_product_description sspd,sc_shop_product_attribute sspa where ssp.id = sspd.product_id AND ssp.id= sspa.product_id AND sspd.product_id=sspa.product_id AND ssp.sku=" + skuString
     console.log("hello DB query is",dbQyery);
@@ -67,7 +67,10 @@ app.get("/getskudetails",(req,res) => {
             return res.status(404).send("no data found")
         }
 
+    
     });
+
+
 });
 
 
@@ -75,6 +78,10 @@ app.get("/getskudetails",(req,res) => {
 
 
 app.get("/getinventory",(req,res) => {
+
+
+    var arraySizes= ["XS_size","S_size"," M_size", "L_size","XL_size","XXL_size"]
+    if(arraySizes.includes(req.query.size)){
 
 console.log("hello inventory", req.query);
     var skuString = "'" +  req.query.skuID + "'";
@@ -95,6 +102,13 @@ console.log("hello inventory", req.query);
         }
 
     });
+}
+
+else{
+    return res.status(404).send("enter valid size")
+
+}
+
 });
 const port = process.env.SYSTEM_PORT || 8080
 

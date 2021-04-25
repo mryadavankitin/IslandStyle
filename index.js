3d7551d38ae3d9bb4e7d07c1733cb16e63f16452
mryadavankitin <mryadavankitin@gmail.com>
2021-04-25 16:45:14 +0530

    Added Connection Pool

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const mysql = require('mysql')
 
 //pass: Ga@5xIfjxWVFbmVT
 //create connection
-const db = mysql.createConnection({
-    host: process.env.host,
-    user: process.env.user,
-    password: process.env.password,
-    database : process.env.database,
-    port:process.env.port
 
-})
+// const db = mysql.createConnection({
+//     host: process.env.host,
+//     user: process.env.user,
+//     password: process.env.password,
+//     database : process.env.database,
+//     port:process.env.port
+
+// })
 
 // let pool = mysql.createPool(db);
 
@@ -20,6 +21,8 @@ const db = mysql.createConnection({
 //         _conn.query('SET SESSION auto_increment_increment=1');
 //     }
 // });
+
+
 // const db = mysql.createConnection({
 //     host: "localhost",
 //     user: "styleisland",
@@ -29,122 +32,205 @@ const db = mysql.createConnection({
 
 // })
 
-
-
-function handleDisconnect() {
-    connection = mysql.createConnection(db); // Recreate the connection, since
-                                                    // the old one cannot be reused.
+var db  = mysql.createPool({
+    connectionLimit : 10,
+    host: "localhost",
+    user: "styleisland",
+    password: "Ga@5xIfjxWVFbmVT",
+    database : "styleisland",
+    port:"3306"
+  });
   
-    connection.connect(function(err) {              // The server is either down
-      if(err) {                                     // or restarting (takes a while sometimes).
-        console.log('error when connecting to db:', err);
-        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-      }                                     // to avoid a hot loop, and to allow our node script to
-    });                                     // process asynchronous requests in the meantime.
-                                            // If you're also serving http, display a 503 error.
-    connection.on('error', function(err) {
-      console.log('db error', err);
-      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-        handleDisconnect();                         // lost due to either server restart, or a
-      } else {                                      // connnection idle timeout (the wait_timeout
-        throw err;                                  // server variable configures this)
-      }
-    });
-  }
+
+
+// function handleDisconnect() {
+//     connection = mysql.createConnection(db); // Recreate the connection, since
+//                                                     // the old one cannot be reused.
+  
+//     connection.connect(function(err) {              // The server is either down
+//       if(err) {                                     // or restarting (takes a while sometimes).
+//         console.log('error when connecting to db:', err);
+//         setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+//       }                                     // to avoid a hot loop, and to allow our node script to
+//     });                                     // process asynchronous requests in the meantime.
+//                                             // If you're also serving http, display a 503 error.
+//     connection.on('error', function(err) {
+//       console.log('db error', err);
+//       if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+//         handleDisconnect();                         // lost due to either server restart, or a
+//       } else {                                      // connnection idle timeout (the wait_timeout
+//         throw err;                                  // server variable configures this)
+//       }
+//     });
+//   }
 
 
 
 
 //connect to mySQL
 
-db.connect(err =>{
+// db.connect(err =>{
 
-    console.log("db host", process.env.host)
-    console.log("db password", process.env.password)
+//     console.log("db host", process.env.host)
+//     console.log("db password", process.env.password)
 
-    if(err){
-        handleDisconnect();
+//     if(err){
+//         // handleDisconnect();
 
-        throw err
+//         throw err
 
-    }
-console.log("my sql connected")
-})
+//     }
+// console.log("my sql connected")
+// })
 
 const app = express()
 
-app.get("/getskudetails",(req,res) => {
 
-    
+app.get("/getskudetails",(req,res) => {
     var skuString = "'" +  req.query.skuID + "'";
     var dbQyery = "Select sspd.name productName,sspa.name sizes, ssp.id, ssp.price,ssp.sku from sc_shop_product ssp, sc_shop_product_description sspd,sc_shop_product_attribute sspa where ssp.id = sspd.product_id AND ssp.id= sspa.product_id AND sspd.product_id=sspa.product_id AND ssp.sku=" + skuString
     console.log("hello DB query is",dbQyery);
-    db.query(dbQyery, (err, rows) => {
-        if(err) throw err;
-        console.log('The data from users table are: \n', rows);
-        // db.end();
-        if(rows.length >0){
-            var sizeAttr=[]
-            for(var i=0; i < rows.length ; i++){
 
-               sizeAttr.push(rows[i].sizes)
+    db.getConnection((err, connection) => {
+        if(err) throw err;
+        console.log('connected as id ' + connection.threadId);
+        connection.query(dbQyery, (err, rows) => {
+            connection.release(); // return the connection to pool
+            if(err) throw err;
+
+            if(rows.length >0){
+                var sizeAttr=[]
+                for(var i=0; i < rows.length ; i++){
+    
+                   sizeAttr.push(rows[i].sizes)
+                }
+    
+                var ResponeDict= rows[0];
+    
+                ResponeDict.sizes= sizeAttr;
+                return res.status(200).send(ResponeDict)
+    
+            }
+            else{
+                return res.status(404).send("no data found")
             }
 
-            var ResponeDict= rows[0];
-            console.log("sixe att above", ResponeDict)
+        });
+    });
+});
 
-            ResponeDict.sizes= sizeAttr;
-            console.log("sixe att", ResponeDict)
-            return res.status(200).send(ResponeDict)
 
-        }
-        else{
-            return res.status(404).send("no data found")
-        }
+
+// app.get("/getskudetails",(req,res) => {
 
     
-    });
+//     var skuString = "'" +  req.query.skuID + "'";
+//     var dbQyery = "Select sspd.name productName,sspa.name sizes, ssp.id, ssp.price,ssp.sku from sc_shop_product ssp, sc_shop_product_description sspd,sc_shop_product_attribute sspa where ssp.id = sspd.product_id AND ssp.id= sspa.product_id AND sspd.product_id=sspa.product_id AND ssp.sku=" + skuString
+//     console.log("hello DB query is",dbQyery);
+//     db.query(dbQyery, (err, rows) => {
+//         if(err) throw err;
+//         console.log('The data from users table are: \n', rows);
+//         // db.end();
+//         if(rows.length >0){
+//             var sizeAttr=[]
+//             for(var i=0; i < rows.length ; i++){
+
+//                sizeAttr.push(rows[i].sizes)
+//             }
+
+//             var ResponeDict= rows[0];
+//             console.log("sixe att above", ResponeDict)
+
+//             ResponeDict.sizes= sizeAttr;
+//             console.log("sixe att", ResponeDict)
+//             return res.status(200).send(ResponeDict)
+
+//         }
+//         else{
+//             return res.status(404).send("no data found")
+//         }
+
+    
+//     });
 
 
-});
+// });
 
 
 //Fectch Inventory Details
-
-
 app.get("/getinventory",(req,res) => {
-
-
     var arraySizes= ["XS_size","S_size"," M_size", "L_size","XL_size","XXL_size"]
     if(arraySizes.includes(req.query.size)){
+        console.log("hello inventory", req.query);
+        var skuString = "'" +  req.query.skuID + "'";
+        var dbQyery = "SELECT " + req.query.size + " from productinventory where sku=" + skuString;
+        // var dbQyery = "SELECT" + "${ req.query.size }" + "from productinventory where sku= ";
 
-console.log("hello inventory", req.query);
-    var skuString = "'" +  req.query.skuID + "'";
-    var dbQyery = "SELECT " + req.query.size + " from productinventory where sku=" + skuString;
-    // var dbQyery = "SELECT" + "${ req.query.size }" + "from productinventory where sku= ";
-    console.log("hello query",dbQyery);
-
-    db.query(dbQyery, (err, rows) => {
+        
+    db.getConnection((err, connection) => {
         if(err) throw err;
-        console.log('The data from users table are: \n', rows);
-        // db.end();
-        if(rows.length >0){
-            return res.status(200).send(rows)
+        console.log('connected as id ' + connection.threadId);
+        connection.query(dbQyery, (err, rows) => {
+            connection.release(); // return the connection to pool
+            if(err) throw err;
 
-        }
-        else{
-            return res.status(404).send("no data found")
-        }
+            if(rows.length >0){
+                return res.status(200).send(rows)
+    
+            }
+            else{
+                return res.status(404).send("no data found")
+            }
+    
 
+        });
     });
-}
 
+}
 else{
     return res.status(404).send("enter valid size")
 
-}
 
+}
 });
+
+
+
+// app.get("/getinventory",(req,res) => {
+
+
+//     var arraySizes= ["XS_size","S_size"," M_size", "L_size","XL_size","XXL_size"]
+//     if(arraySizes.includes(req.query.size)){
+
+// console.log("hello inventory", req.query);
+//     var skuString = "'" +  req.query.skuID + "'";
+//     var dbQyery = "SELECT " + req.query.size + " from productinventory where sku=" + skuString;
+//     // var dbQyery = "SELECT" + "${ req.query.size }" + "from productinventory where sku= ";
+//     console.log("hello query",dbQyery);
+
+//     db.query(dbQyery, (err, rows) => {
+//         if(err) throw err;
+//         console.log('The data from users table are: \n', rows);
+//         // db.end();
+//         if(rows.length >0){
+//             return res.status(200).send(rows)
+
+//         }
+//         else{
+//             return res.status(404).send("no data found")
+//         }
+
+//     });
+// }
+
+// else{
+//     return res.status(404).send("enter valid size")
+
+// }
+
+// });
+
+
 const port = process.env.SYSTEM_PORT || 8080
 
 app.listen(port, () => {
